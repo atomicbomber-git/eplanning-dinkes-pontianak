ee2a78f4288af6970f4834c065409fb7ba18ff12
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-09-05 14:34:28 +0700

    Updates.

--- resources/views/puskesmas/rencana-lima-tahunan/index.blade.php
+++ resources/views/puskesmas/rencana-lima-tahunan/index.blade.php
@@ -65,7 +65,6 @@
                                                 }
                                             })
 "
-
                                 >
                                     @csrf
                                     @method("DELETE")
